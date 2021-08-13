8d3f25564533034740547cf24ccebf42b2c5a086
VladLinkevich <45914784+VladLinkevich@users.noreply.github.com>
2021-08-13 13:49:08 +0300

    Storing units

---
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using Mirror;
+using Units;
+using UnityEngine;
+
+namespace Networking
+{
+    public class RTSPlayer : NetworkBehaviour
+    {
+        private List<Unit> _units = new List<Unit>();
+
+        #region Server
+        
+        public override void OnStartServer()
+        {
+            Unit.ServerOnUnitSpawned += Unit_ServerOnUnitSpawned;
+            Unit.ServerOnUnitDespawned += Unit_ServerOnUnitDespawned;
+        }
+
+        public override void OnStopServer()
+        {
+            Unit.ServerOnUnitSpawned -= Unit_ServerOnUnitSpawned;
+            Unit.ServerOnUnitDespawned -= Unit_ServerOnUnitDespawned;
+        }
+
+        private void Unit_ServerOnUnitSpawned(Unit unit)
+        {
+            if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+            
+            _units.Add(unit);
+        }
+
+        private void Unit_ServerOnUnitDespawned(Unit unit)
+        {
+            if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+            _units.Remove(unit);
+        }
+
+        #endregion
+        
+        #region Client
+
+        public override void OnStartClient()
+        {
+            if (isClientOnly == false) { return; }
+            
+            Unit.AuthorityOnUnitSpawned += Unit_AuthorityOnUnitSpawned;
+            Unit.AuthorityOnUnitDespawned += Unit_AuthorityOnUnitDespawned;
+        }
+
+        public override void OnStopClient()
+        {
+            if (isClientOnly == false) { return; }
+            
+            Unit.AuthorityOnUnitSpawned -= Unit_AuthorityOnUnitSpawned;
+            Unit.AuthorityOnUnitDespawned -= Unit_AuthorityOnUnitDespawned;
+        }
+
+        private void Unit_AuthorityOnUnitSpawned(Unit unit)
+        { 
+            if (hasAuthority == false) { return; }
+            _units.Add(unit);
+        }
+
+        private void Unit_AuthorityOnUnitDespawned(Unit unit)
+        {
+            if (hasAuthority == false) { return; }
+            _units.Remove(unit);
+        }
+
+        #endregion
+    }
+}
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,3 +1,4 @@
+using System;
 using Mirror;
 using UnityEngine;
 
@@ -8,9 +9,43 @@ namespace Units
         [SerializeField] private UnitMovement UnitMovement = null;
         [SerializeField] private GameObject SelectedHighlighting;
 
+        public static event Action<Unit> ServerOnUnitSpawned;
+        public static event Action<Unit> ServerOnUnitDespawned;
+        
+        public static event Action<Unit> AuthorityOnUnitSpawned;
+        public static event Action<Unit> AuthorityOnUnitDespawned;
+        
         public UnitMovement GetUnitMovement => UnitMovement;
         
+        #region Server
+
+        public override void OnStartServer()
+        {
+            ServerOnUnitSpawned?.Invoke(this);
+        }
+
+        public override void OnStopServer()
+        {
+            ServerOnUnitDespawned?.Invoke(this);
+        }
+
+        #endregion
+        
         #region Client
+
+        public override void OnStartClient()
+        {
+            if (isClientOnly == false || hasAuthority == false) { return; }
+            
+            AuthorityOnUnitSpawned?.Invoke(this);
+        }
+        
+        public override void OnStopClient()
+        {
+            if (isClientOnly == false || hasAuthority == false) { return; }
+
+            AuthorityOnUnitDespawned?.Invoke(this);
+        }
 
         [Client]
         public void Select()
